6ca10fafafa48c9b0d5c09558fd785771d5f4c99
Elida <elidayanti07@gmail.com>
2021-10-12 13:03:19 +0700

    first commit

---
+++ bio.java
@@ -0,0 +1,20 @@
+public class bio {
+    public static void main(String[] args){
+    String firstName = "Elida";
+    String lastName = "Yanti";
+    String fullName = "Elida Yanti";
+    int age = 21;
+    String addres = "Medan";
+    String numberPhone = "085761845024";
+    String hobby = "Vollyball";
+    String favoriteFood = "Meatball";
+
+System.out.println ("FirstName :" + firstName);
+System.out.println("LastName :" + lastName);
+System.out.println("FullName :" + fullName);
+System.out.println ("Age :" + age);
+System.out.println("Addres :" + addres);
+System.out.println("NumberPhone :" + numberPhone);
+System.out.println("Hobby :" + hobby);
+System.out.println("FavoriteFood :" + favoriteFood);
+}}
